8bdbdc8a748cb24033649e2a2db1ecdfa6dcebc2
Micah Nuske <0349460@schoolsnet.act.edu.au>
2026-03-30 13:31:20 +1100

    Prototype Submission

--- InClassDjango/MyApp/views.py
+++ InClassDjango/MyApp/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime 
+from django.shortcuts import render, redirect
 from .models import teacher
+from .forms import InputForm
 
 # Create your views here.
 def index(request):
     
     teach = teacher.objects.all()
     return render(request, "MyApp/index.html", {'content' :teach})
+
+
+def input_view(request):
+    if request.method == "Post":
+        form = InputForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("indext")
+        else:
+            form = InputForm()
+
+            return render(request, "MyApp/input.html", {"form": form})
